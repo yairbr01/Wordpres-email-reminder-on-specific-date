430d65ce360a5bf0dab51f9a9ce0655c88f6afda
Yair Beoyer <36797112+yairbr01@users.noreply.github.com>
2022-01-16 12:41:57 +0200

    Update send_email_reminders.php

--- send_email_reminders.php
+++ send_email_reminders.php
@@ -22,19 +22,19 @@ function send_email_reminders() {
 	$today_timestamp = time();
 	
 	// WP_Agent_Query arguments
-    $args_weekly = array(
-        'fields' => 'ids',
-        'post_status' => 'publish',
-        'post_type'  => 'enter_your_post_type',
-        'posts_per_page' => -1,
-        'meta_key'     => 'validity_date',
-        'meta_value'   => $today_timestamp,
-        'type'    => 'numeric',
-        'meta_compare' => '=',
-    );
+	$args_weekly = array(
+		'fields' => 'ids',
+		'post_status' => 'publish',
+		'post_type'  => 'enter_your_post_type',
+		'posts_per_page' => -1,
+		'meta_key'     => 'validity_date',
+		'meta_value'   => $today_timestamp,
+		'type'    => 'numeric',
+		'meta_compare' => '=',
+	);
     
-    // The Agent Query
-    $email_reminders_query = new WP_Query( $args_weekly );
+	// The Agent Query
+	$email_reminders_query = new WP_Query( $args_weekly );
 	
 	if ( $email_reminders_query->have_posts() ) {
 		
@@ -43,56 +43,56 @@ function send_email_reminders() {
 		foreach ( $email_reminders_query->posts as $post ) {
 			//Get the author email
 			$author_id = get_post_field( 'post_author', $post );
-            $authr_email = get_the_author_meta( 'user_email', $author_id );
+			$authr_email = get_the_author_meta( 'user_email', $author_id );
 			$email_addresses[] = $authr_email;
 		}
 		
 		//Setting up email settings (email address is set below).
-        $from = "no_reply@example.com";
-        $from_name = "Sender name";
-        $subject = "message subject";
+		$from = "no_reply@example.com";
+		$from_name = "Sender name";
+		$subject = "message subject";
 	    
-	    $html_email_template_file = get_stylesheet_directory_uri().'/html/send_email_template.html';
-	    $message = file_get_contents($html_email_template_file);
+		$html_email_template_file = get_stylesheet_directory_uri().'/html/send_email_template.html';
+		$message = file_get_contents($html_email_template_file);
 	    
-	    $to = $email_addresses;
+		$to = $email_addresses;
 	
-    	if (!empty($email_addresses)) {
-            custom_send_mass_mail_no_reply_account($from,$from_name,$subject,$message,$to);
-        }
+		if (!empty($email_addresses)) {
+			custom_send_mass_mail_no_reply_account($from,$from_name,$subject,$message,$to);
+		}
 	}
 }
 
 //Email Reminders PHPMailer
 function custom_send_mass_mail_no_reply_account($from,$from_name,$subject,$message,$to) {
     
-    $mail = new PHPMailer(true);
+	$mail = new PHPMailer(true);
 
-    //Server settings
-    $mail->isSMTP();
-    $mail->Host       = 'mail.example.com';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'no_reply@example.com';
-    $mail->Password   = 'your_password';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port       = 465;
-    $mail->CharSet   = 'utf-8';
+	//Server settings
+	$mail->isSMTP();
+	$mail->Host       = 'mail.example.com';
+	$mail->SMTPAuth   = true;
+	$mail->Username   = 'no_reply@example.com';
+	$mail->Password   = 'your_password';
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+	$mail->Port       = 465;
+	$mail->CharSet   = 'utf-8';
     
-    //Recipients
-    $mail->setFrom($from, $from_name);
+	//Recipients
+	$mail->setFrom($from, $from_name);
     
-    //Send a message to any email address in the array
-    if(is_array($to)) {
-        foreach($to as $t) {
-            $mail->addBCC($t);
-        }
-    }
+	//Send a message to any email address in the array
+	if(is_array($to)) {
+		foreach($to as $t) {
+			$mail->addBCC($t);
+		}
+	}
 
-    //Content
-    $mail->isHTML(true);
-    $mail->Subject = $subject;
-    $mail->Body    = $message;
+	//Content
+	$mail->isHTML(true);
+	$mail->Subject = $subject;
+	$mail->Body    = $message;
 
-    $mail->send();
+	$mail->send();
     
 }
